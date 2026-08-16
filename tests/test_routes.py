01b099112fe998ae800a2e6840334416580ade91
Zeeshan Anwar <ishanrajpoot78@gmail.com>
2026-08-16 15:15:21 +0500

    Add test_get_account_not_found to test routes
    Add test for handling account not found scenario

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -140,12 +140,15 @@ class TestAccountService(TestCase):
         """It should not allow an illegal method call"""
         resp = self.client.delete(BASE_URL)
         self.assertEqual(resp.status_code, status.HTTP_405_METHOD_NOT_ALLOWED)
-
-
     # ----------------------------------------------------------
     # READ TESTS
     # ----------------------------------------------------------
     
+    def test_get_account_not_found(self):
+        """It should not Read an Account that is not found"""
+        resp = self.client.get(f"{BASE_URL}/0")
+        self.assertEqual(resp.status_code, status.HTTP_404_NOT_FOUND)
+
 
     # ----------------------------------------------------------
     # UPDATE TESTS
